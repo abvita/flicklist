080581296919723e4c4f13ae1b79c4426f70a7f5
Alexa Vita <alexvita2@gmail.com>
2017-03-27 19:12:56 -0400

    implemented styling and search function, etc

--- js/flicklist.js
+++ js/flicklist.js
@@ -14,7 +14,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 add your api key
+  token: "d20fcf3f28266a495abf2e00220f92fc" // TODO 0 add your api key
 }
 
 
@@ -32,6 +32,7 @@ function discoverMovies(callback) {
     },
     success: function(response) {
       model.browseItems = response.results;
+      //console.log(model.watchlistItems);
       callback();
     }
   });
@@ -39,7 +40,7 @@ function discoverMovies(callback) {
 
 
 /**
- * Makes an AJAX request to the /search/movie endpoint of the API, using the 
+ * Makes an AJAX request to the /search/movie endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, updates model.browseItems appropriately and then invokes
@@ -51,10 +52,19 @@ function searchMovies(searchTerm, callback) {
   // TODO 9
   // implement this function as described in the comment above
   // you can use the body of discoverMovies as a jumping off point
-
+  $.ajax({
+    url: api.root + "/search/movie",
+    data: {
+      api_key: api.token,
+      query: searchTerm
+    },
+    success: function(response) {
+      model.browseItems = response.results;
+      callback();
+    }
+  });
 
 }
-
 
 /**
  * re-renders the page with new content, based on the current state of the model
@@ -72,6 +82,7 @@ function render() {
       .append(title)
       // TODO 3
       // give itemView a class attribute of "item-watchlist"
+      .attr("class", "item-watchlist");
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -90,25 +101,25 @@ function render() {
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
 
+      if (model.watchlistItems.indexOf(movie) != -1){
+        button.prop("disabled", true);
+      }
 
     // TODO 1
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
-
+    var description = $("<p></p>").text(movie.overview);
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(description)
       .append(button);
 
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
-  
+
 }
-
-
-
-
